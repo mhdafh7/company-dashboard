d784cd48f382963c3030cf9ef120abeaa5dcb290
Mhdafh <mhdafh@tuta.io>
2023-05-14 17:57:12 +0530

    add image domain

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["localhost", "cloudflare-ipfs.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
